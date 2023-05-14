7706fa2f412829a92b36601af132fee8e3a22069
Zareel Kalam <zareelbwd@gmail.com>
2023-05-14 10:46:19 +0530

    collection schema

--- controllers/authConroller.js
+++ controllers/authConroller.js
@@ -7,7 +7,7 @@ export const signUp = async (req, res) => {
   const { name, email, password, phone, address } = req.body;
   //validation
   if (!name || !email || !password || !phone || !address) {
-    throw new Error("Required field");
+    return res.send({ error: "requiured all fields" });
   }
   try {
     //check if the user already exists
@@ -44,46 +44,6 @@ export const signUp = async (req, res) => {
   }
 };
 
-// login || method:post
-/*
-export const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    //validate
-    if (!email || !password) {
-      return res.status(404).send({
-        success: false,
-        message: "Invalid email or password",
-      });
-    }
-    //check user
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).send({
-        success: false,
-        message: "Email is not registered",
-      });
-    }
-    //compare password
-    const match = await comparePassword(password, user.password);
-    if (!match) {
-      return res.status(200).send({
-        success: false,
-        message: "Invalid password",
-      });
-    }
-    // token
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      success: false,
-      message: "Error in login",
-      error,
-    });
-  }
-};
-*/
-
 export const login = async (req, res) => {
   try {
     // get user details
@@ -111,23 +71,24 @@ export const login = async (req, res) => {
         success: false,
         message: "Invalid password",
       });
+    } else {
+      //token
+      const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "14d",
+      });
+      res.status(200).send({
+        success: true,
+        message: "login successfully",
+        user: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          phone: user.phone,
+          adddress: user.address,
+        },
+        token,
+      });
     }
-    //token
-    const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
-    res.status(200).send({
-      success: true,
-      message: "login successfully",
-      user: {
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        phone: user.phone,
-        adddress: user.address,
-      },
-      token,
-    });
   } catch (error) {
     console.log(error);
     res.status(500).send({
@@ -136,4 +97,10 @@ export const login = async (req, res) => {
       error,
     });
   }
+};
+
+//test controller
+
+export const testController = (req, res) => {
+  res.send("protected route");
 };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { signUp, login } from "../controllers/authConroller.js";
+import { signUp, login, testController } from "../controllers/authConroller.js";
+import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
 
 //router object
 const router = express.Router();
@@ -11,4 +12,6 @@ router.post("/signup", signUp);
 //login || POST
 router.post("/login", login);
 
+//test || get
+router.get("/test", requireSignIn, isAdmin, testController);
 export default router;
